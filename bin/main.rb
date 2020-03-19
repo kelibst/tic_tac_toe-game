eccc0b674a7549515ed9d9976cfbf59fbea4e17a
kelibst <kbooster17@gmail.com>
2020-03-19 14:29:03 +0000

    milestone 2 done

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,28 @@
 #!/usr/bin/env ruby
-puts "Hello World!"
+
+puts "Player1 starts!\n Make a move with number from 1 to 9"
+
+player1_input = gets.chomp
+
+nums = "1"..."9"
+nums.include?(player1_input)
+
+puts "Pleae enter a valid nummber between 0 to 9" unless nums.include?(player1_input)
+puts "Here is the dastboard.
+----
+----
+----
+"
+puts "Awesome! now it is player2 's turn."
+
+player2_input = gets.chomp
+
+nums.include?(player2_input)
+
+puts "Here is the dastboard.
+----
+----
+----
+"
+
+puts "player 2 won!!!"
