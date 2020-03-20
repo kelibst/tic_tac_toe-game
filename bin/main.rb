9a979ba1abcd4c609133b6d432fe35f2f5d0e67c
kelibst <kbooster17@gmail.com>
2020-03-20 16:08:48 +0000

    Fixed stickler and some weird player 2 input repeat bug

--- bin/main.rb
+++ bin/main.rb
@@ -1,55 +1,44 @@
 #!/usr/bin/env ruby
 
-loop do 
-    puts "Ready? \n Enter n to exit the game!"
+dashboard_arr = %w[1 2 3 4 5 6 7 8 9]
 
-    play = gets.chomp
-
-    puts "Exiting game!!!" if play.downcase == "n"
-    break if play.downcase == "n"
-
-    dashboard_Arr = ["1","2","3","4","5","6","7","8","9"]
-
-    def dashboard (board)
-
-        puts " #{board[0]} | #{board[1]} | #{board[2]} "
-        puts "-----------"
-        puts " #{board[3]} | #{board[4]} | #{board[5]} "
-        puts "-----------"
-        puts " #{board[6]} | #{board[7]} | #{board[8]} "
-        
-    end
-
-   
-
-    for x in dashboard_Arr
-        nums = "1".."9"
-        
-        player = 1 if x.to_i.odd?
-        player = 2 if x.to_i.even?
-       
-       puts " ========================
- ========================"
-        loop do 
-        puts "Player#{player}\n Make a move with number from 1 to 9"
-        input =gets.chomp
-        puts input
-        puts "Your move is not valid!" unless nums.include?(input)
-        
-        
-        if player ==1 && nums.include?(input) 
-            dashboard_Arr[input.to_i - 1] = "X"  
-        elsif player ==2 && nums.include?(input)
-             dashboard_Arr[input.to_i - 1] = "O"
-        end
-            dashboard(dashboard_Arr)
-        break if nums.include?(input)
-        end
-        
-        
-    end
-
-
+def dashboard(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+loop do
+  puts "Ready? \n Enter n to exit the game!"
 
+  play = gets.chomp
+
+  puts 'Exiting game!!!' if play.downcase == 'n'
+  break if play.downcase == 'n'
+
+  dashboard_arr.each do |x|
+    nums = '1'..'9'
+
+    player = 1 if x.to_i.odd?
+    player = 2 if x.to_i.even?
+
+    puts " ========================
+ ========================"
+    loop do
+      puts "Player#{player}\n Make a move with number from 1 to 9"
+      input = gets.chomp
+      puts input
+      puts 'Your move is not valid!' unless nums.include?(input)
+
+      if player == 1 && nums.include?(input)
+        dashboard_arr[input.to_i - 1] = 'X'
+      elsif player == 2 && nums.include?(input)
+        dashboard_arr[input.to_i - 1] = 'O'
+      end
+      dashboard(dashboard_arr)
+      break if nums.include?(input)
+    end
+  end
+end
