98b110cf9bc3515760d83f8643816c30bb72e8cb
mbabali iryn <mbabaliiryn@gmail.com>
2020-03-20 11:37:57 +0300

    implement the user_interface and logic of the tic_tac_toe game

--- bin/main.rb
+++ bin/main.rb
@@ -4,10 +4,10 @@ puts "Player1 starts!\n Make a move with number from 1 to 9"
 
 player1_input = gets.chomp
 
-nums = "1"..."9"
+nums = '1'...'9'
 nums.include?(player1_input)
 
-puts "Pleae enter a valid nummber between 0 to 9" unless nums.include?(player1_input)
+puts 'Pleae enter a valid nummber between 0 to 9' unless nums.include?(player1_input)
 puts "Here is the dastboard.
 ----
 ----
@@ -25,4 +25,4 @@ puts "Here is the dastboard.
 ----
 "
 
-puts "player 2 won!!!"
+puts 'player 2 won!!!'
---
+++ test.rb
@@ -0,0 +1,4 @@
+require_relative 'user_interface.rb'
+WIN = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
+
+Game.start
